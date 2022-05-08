41b23e10421e9ab241b65144aa4befc74c677397
Samuel Souza <augusto_souza@edu.unifil.br>
2022-05-07 21:05:49 -0300

    Component StartScreen

---
+++ src/components/StartScreen.js
@@ -0,0 +1,14 @@
+import "./StartScreen.css";
+
+import React from "react";
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+      <h1>Secret Word</h1>
+      <button>Start</button>
+    </div>
+  );
+};
+
+export default StartScreen;
