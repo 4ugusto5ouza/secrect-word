382ac755c19b84d1991753c7481952b05267c909
Samuel Souza <augusto_souza@edu.unifil.br>
2022-05-07 21:20:43 -0300

    Game e EndScreen criados

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import "./App.css";
-import StartScreen from "./components/StartScreen";
+// react
+import { useCallback, useEffect, useState } from "react";
 
-function App() {
+// styles
+import "./App.css";
+
+//components
+import StartScreen from "./components/StartScreen";
+import Game from "./components/Game";
+import EndScreen from "./components/EndScreen";
+
+// data
+import { wordsList } from "./data/words";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
+const App = () => {
+  const [gameStage, setGameStage] = useState(stages[0].id);
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === stages[0].id && <StartScreen />}
+      {gameStage === stages[1].id && <Game />}
+      {gameStage === stages[2].id && <EndScreen />}
     </div>
   );
-}
+};
 
 export default App;
